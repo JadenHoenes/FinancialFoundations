9998a58277276f625663422fbff46048a7bc3f70
Jaden Hoenes <jadhoenes@gmail.com>
2018-12-11 19:49:55 -0700

    login react page, using axios

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.loginUser = this.loginUser.bind(this)
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section>
+          <form onSubmit={this.loginUser}>
+            <fieldset>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="text"
+                  name="email"
+                  value={this.state.email}
+                  onChange={e => this.onEmailChange(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={e => this.onPasswordChange(e.target.value)}
+                />
+              </div>
+              <div>
+                <button type="submit">Login</button>
+              </div>
+            </fieldset>
+          </form>
+        </section>
       </div>
-    );
+    )
+  }
+
+  onEmailChange(email) {
+    //this.state.email = email;
+    this.setState({email: email})
+  }
+
+  onPasswordChange(pass) {
+    //this.state.email = email;
+    this.setState({password: pass})
+  }
+
+  loginUser(event) {
+    event.preventDefault()
+    console.log(event, this.state)
+    debugger
+    axios
+      .post('http://localhost:3000/api/users/login', {
+        email: this.state.email,
+        password: this.state.password,
+      })
+      .then(res => {
+        console.log(res)
+      })
   }
 }
 
-export default App;
+export default App
