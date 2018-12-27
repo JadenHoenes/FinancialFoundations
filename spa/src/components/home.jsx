7d1b9421d3fa4e3bcfc38bc9273889d2606fbaaa
Corbin Hoenes <corbin.hoenes@domo.com>
2018-12-26 17:46:01 -0700

    randomize image, update home text to match mocks

--- spa/src/components/home.jsx
+++ spa/src/components/home.jsx
@@ -2,11 +2,29 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 class Home extends Component {
+  images = ['home.jpg', 'home1.jpg'];
+
+  constructor(props) {
+    super(props);
+    this.styles = this.getRandomImage();
+  }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min+1)) + min;
+  }
+
+  getRandomImage() {
+    const index = this.getRandomInt(0, this.images.length-1);
+    return {
+      background: `url('${this.images[index]}') no-repeat scroll center 0px / cover`
+    }
+  }
+
   state = {}
   render() {
     return (
       <div>
-        <div className="masthead">
+        <div className="masthead" style={this.styles}>
           <div className="pitch">
             <div className="ui text container">
               <div className="logocontainer">
@@ -31,11 +49,15 @@ class Home extends Component {
           <div className="ui middle aligned stackable grid container">
             <div className="row">
               <div className="eight wide column">
-                <h3 className="ui header">We Help Companies and Companions</h3>
+                <h3 className="ui header">Planning Done Right</h3>
                 <p>
-                  We can give your company superpowers to do things that they
-                  never thought possible. Let us delight your customers and
-                  empower your needs...through pure data analytics.
+                  Your clients need a true financial advisor, not just someone who can tell them about a product
+                  or two or manage an investment for them. They need comprehensive financial planning.
+                  That’s why they work with you. You cover everything from financial protection to retirement planning and everything in between.
+                  As important as all of this is, it takes time and effort. With Financial Foundations&trade; you can do it all faster and more efficiently
+                  than ever before, allowing you to help more people, faster. Financial Foundations&trade; automates the process for you,
+                  helping your clients see the value behind your suggestions and advice.
+                  Financial Foundations&trade; has the tools you need to help your clients and all those they will want to refer you to.
                 </p>
                 <h3 className="ui header">We Make Bananas That Can Dance</h3>
                 <p>
@@ -45,9 +67,8 @@ class Home extends Component {
               </div>
               <div className="six wide right floated column">
                 <img
-                  src="assets/images/wireframe/white-image.png"
+                  src="img.png"
                   className="ui large bordered rounded image"
-                  alt="Error: not found"
                 />
               </div>
             </div>
