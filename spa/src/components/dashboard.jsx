2b8b80d23ce3cbb9e01f898582fe77e97d728381
Jaden Hoenes <jadhoenes@gmail.com>
2018-12-27 13:06:26 -0700

    building out pages

--- spa/src/components/dashboard.jsx
+++ spa/src/components/dashboard.jsx
@@ -1,10 +1,79 @@
 import React, {Component} from 'react'
+import {Button} from 'semantic-ui-react'
+import './dashboard.css'
 
 class Dashboard extends Component {
   render() {
     return (
-      <div className="mainContainer">
-        <p>ToDo dashboard</p>
+      <div className="dashboard">
+        <div className="flexbox col">
+          <h3 className="welcome">Welcome</h3>
+          <div>
+            The first step in the Financial FoundationsTM process is to create a
+            client profile (aka: have a fact find) for your respective client.
+            This will gather all the information you need to complete each step
+            of the planning process with them. Afterwards, you’ll step through
+            each aspect in it’s place and priority.
+          </div>
+          <div className="flexbox row center">
+            <Button>New Client Profile</Button>
+            <Button className="dashboard">Existing Client Profiles</Button>
+            <Button className="dashboard">Client Presentations</Button>
+          </div>
+          <div className="">
+            <h3>Planning Tools Quick Links</h3>
+            <div className="flexbox row evenly">
+              <div>
+                <ul>
+                  <li>
+                    <a>Life Insurance Needs Analysis Tool</a>
+                  </li>
+                  <li>
+                    <a>Disability Protection Analysis Tool</a>
+                  </li>
+                  <li>
+                    <a>Property & Casualty Coverage Tool</a>
+                  </li>
+                  <li>
+                    <a>Health Insurance Coverage</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    <a>Retirement Income Plan Optimizer</a>
+                  </li>
+                  <li>
+                    <a>Tax-Free Retirement Calculator</a>
+                  </li>
+                  <li>
+                    <a>Long-Term Care Calculator</a>
+                  </li>
+                  <li>
+                    <a>Investment Tracker</a>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    <a>Emerg. Fund / Debt Payoff Calculator</a>
+                  </li>
+                  <li>
+                    <a>Mortgage Payoff Calculator</a>
+                  </li>
+                  <li>
+                    <a>Social Sec. Optimization Calculator</a>
+                  </li>
+                  <li>
+                    <a>Medicare Calculator</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- spa/src/components/overview.jsx
+++ spa/src/components/overview.jsx
@@ -2,8 +2,29 @@ import React, {Component} from 'react'
 class Overview extends Component {
   render() {
     return (
-      <div>
-        <p>ToDo overview page</p>
+      <div className="overviewContainer">
+        <div className="textContainer">
+          <p className="textStuff">
+            Financial Foundations™ offers not only a suite of tools you can use
+            to help your clients track through your planning process and see the
+            value in what you are suggesting, but it offers and accommodates a
+            systematic approach to comprehensive financial planning, from asset
+            protection to retirement planning and everything in between.
+            Financial Foundations™ is for the advisor who isn’t content with
+            being deemed a product salesman. Our system and tools were built by
+            individuals working in the same industry. Financial Foundations™
+            allows you to create and maintain client profiles for all your
+            clients and their respective needs, following the systematic
+            approach to achieving financial success and protection in the
+            present, near future, and distant future. Learn more about each step
+            of the system by clicking on the respective element in the
+            illustration on this page.
+          </p>
+        </div>
+        <div className="dividerOverview" />
+        <div className="slideDeck">
+          <img />
+        </div>
       </div>
     )
   }
