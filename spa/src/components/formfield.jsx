0a39081e6d6f5d390e91060aeaf99b48cad3be25
Jaden Hoenes <jadhoenes@gmail.com>
2019-01-22 21:09:38 -0700

    now form field works

--- spa/src/components/formfield.jsx
+++ spa/src/components/formfield.jsx
@@ -2,26 +2,18 @@ import React, {Component} from 'react'
 
 class FormField extends Component {
   state = {}
-  className = ''
-  constructor(props) {
-    super(props)
-    this.className = 'field' + this.props.className || ''
-  }
-
   render() {
     return (
-      <div className={this.className}>
+      <div className={'field '.concat(this.props.className)}>
         <input
           className="ui left labeled input"
           placeholder={this.props.placeholder}
           type="text"
           name={this.props.name}
-          value={this.props.value}
           style={this.props.style}
-          value={this.state.name}
           onChange={e => {
             let field = {}
-            field[this.name] = e.target.value
+            field[this.props.name] = e.target.value
             this.props.onChange(field)
           }}
         />
